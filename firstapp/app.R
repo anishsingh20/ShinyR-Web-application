57edffb29ae79ee78a8724aea1c91455126ec953
Ansih Singh Walia <loveeanish@gmail.com>
2017-06-24 00:25:30 +0530

    Use of some new functions like reactive() and isolate()

--- firstapp/app.R
+++ firstapp/app.R
@@ -19,13 +19,15 @@ ui <- fluidPage(
   
     #*Input() functions 
     #we will identify this input using input$num as a Reactive value
-
+    
     sliderInput(inputId="num",label="Slider Input",value=10,min=0
                 ,max=100) ,
     textInput(inputId="title",label="Input the title") ,
   
     #*Output() functions
-      plotOutput(outputId = "hist")
+      plotOutput(outputId = "hist") ,
+      #for summary statistics of the numbers
+      verbatimTextOutput(outputId = "stats")
    
 )
 
@@ -35,13 +37,25 @@ server<-function(input,output) {
   #Save objects to display to output$
   #render()* builds reactive output to display in UI
   #renderPlot() is a Reactive function which uses a reactive value
+  
+  #building reactive objects using reactive() and use this same data twice below
+  data<-reactive(rnorm(input$num))
+  
   output$hist<-renderPlot({ 
                 #All R code comes inside these {}
+    
+                  #isolate() to make text field non reactive            
+                  hist(rnorm(data()),main=isolate(input$title),
+                       xlab="Numbers",ylab="Count",col="red",border="black",density=-0.5) 
+                          })
+  #outputting the summary statistics defined above in UI
+  output$stats<-renderPrint({summary(data())   
+                        })
                 
-                  hist(rnorm(input$num),main=input$title,
-                       xlab="Numbers",ylab="Count") }
-                )
-}
+
+                  
+                  }#closing server function
+
 
 
 # Run the application using ShinyR
