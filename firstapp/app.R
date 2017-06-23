b62881d4579986267704b7ad11c2c8cf76b50e2b
Ansih Singh Walia <loveeanish@gmail.com>
2017-06-23 16:11:51 +0530

    Added a input to the fluidPage function

--- firstapp/app.R
+++ firstapp/app.R
@@ -7,18 +7,26 @@
 #
 #loading the Shiny Package
 library(shiny)
+#A shiny app Template-
+#UI , Server , ShinyApp 
+
+
 
 #1) Define the UI ie the Desing and Layout of the Application
 #Under the Hood Shiny uses Twitter Bootstrap as a CSS framework
 # Add elements to the app as arguments to fluidPage()
 ui <- fluidPage(
-    {"Hello World"},
-    "This is Anish")
-
-#This is simply a HTML code 
-ui
+    # *Input() functions 
+    sliderInput(inputId="num",label="Slider Input",value=10
+                ,min=0,max=100)
+  
+    # *Output() functions
+      plotOutput(outputId = "Hist")
+    
+)
 
 #2)Define Instructions for the server and how the app will interact 
+#Tell server how to assemble inputs and outputs
 server<-function(input,output) {}
 
 # Run the application using ShinyR
