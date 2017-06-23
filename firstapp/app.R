73a33bfc33a4568e8d667573174169fc469820ee
Ansih Singh Walia <loveeanish@gmail.com>
2017-06-23 17:00:03 +0530

    First Web Application Made

--- firstapp/app.R
+++ firstapp/app.R
@@ -16,18 +16,29 @@ library(shiny)
 #Under the Hood Shiny uses Twitter Bootstrap as a CSS framework
 # Add elements to the app as arguments to fluidPage()
 ui <- fluidPage(
-    # *Input() functions 
-    sliderInput(inputId="num",label="Slider Input",value=10
-                ,min=0,max=100)
   
-    # *Output() functions
-      plotOutput(outputId = "Hist")
-    
+    #*Input() functions 
+    sliderInput(inputId="num",label="Slider Input",value=10,min=0
+                ,max=100) ,
+  
+    #*Output() functions
+      plotOutput(outputId = "hist")
+   
 )
 
 #2)Define Instructions for the server and how the app will interact 
 #Tell server how to assemble inputs and outputs
-server<-function(input,output) {}
+server<-function(input,output) {
+  #Save objects to display to output$
+  #render()* builds reactive output to display in UI
+  output$hist<-renderPlot({ 
+                #All R code comes inside these {}
+                
+                  hist(rnorm(input$num),main="Histogram of 100 Random Normal Numbers",
+                       xlab="Numbers",ylab="Count") }
+                )
+}
+
 
 # Run the application using ShinyR
 ?shinyApp
